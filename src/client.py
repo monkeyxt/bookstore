20e19952668f80e4100b8d7cc1c51b84ce48bfcc
Tian Xia <txia23@amherst.edu>
2021-03-29 17:22:39 -0400

    elevated port number

--- src/client.py
+++ src/client.py
@@ -3,36 +3,41 @@ import sys
 import time
 import yaml
 
-## Load Server configs from yaml
+# Load Server configs from yaml
 with open('config.yml', 'r') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 FRONTEND_IP = config['frontend']
 
-## Search the requested topic
+
+# Search the requested topic
 def search(topic):
     search_output = requests.get(FRONTEND_IP + '/search/' + topic).text
     print(search_output)
     return search_output
 
-## Lookup the requested book item
+
+# Lookup the requested book item
 def lookup(item_number):
     item_number_str = str(item_number)
     lookup_output = requests.get(FRONTEND_IP + '/lookup/' + item_number_str).text
     print(lookup_output)
-    return(lookup_output)
+    return lookup_output
 
-## Buy the requested book item
+
+# Buy the requested book item
 def buy(item_number):
     item_number_str = str(item_number)
     buy_output = requests.get(FRONTEND_IP + '/buy/' + item_number_str).text
     print(buy_output)
-    return(buy_output)
+    return buy_output
+
 
 def main():
     method_name = sys.argv[1]
     parameter_name = sys.argv[2]
     getattr(sys.modules[__name__], method_name)(parameter_name)
+
 
 if __name__ == '__main__':
     main()
