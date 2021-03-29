e9172342b0c02463307e1ec8a552fc57ee036918
Tian Xia <txia23@amherst.edu>
2021-03-29 19:28:33 -0400

    updated frontend

--- src/frontend.py
+++ src/frontend.py
@@ -15,19 +15,19 @@ CATALOG_IP = config['catalog']
 
 
 # Search for the requested topic
-@app.route("/search/<topic>", methods=["GET"])
+@app.route("/search/<topic>")
 def search(topic):
 
     topic_query = {
         "topic": topic
     }
-    books = requests.get(CATALOG_IP + '/query/', json=topic_query).json()
+    books = requests.post(CATALOG_IP + '/query/', json=topic_query).json()
 
     # Parse the json of the search result
     search_result = []
-    for book in books["items"]:
+    for book in books:
         search_result.extend(["Title: ", book, " "])
-        search_result.extend(["ID: ", str(books["items"][book]), "\n"])
+        search_result.extend(["ID: ", str(books[book]['item_number']), "\n"])
 
     # Return the search result in a string
     return "".join(search_result)
@@ -40,14 +40,14 @@ def lookup(item_number):
     item_query = {
         "item_number": item_number
     }
-    books = requests.get(CATALOG_IP + '/query/', json=item_query).json()
+    books = requests.post(CATALOG_IP + '/query/', json=item_query).json()
 
     # Parse the lookup result
     lookup_result = []
     for book in books:
         lookup_result.extend(["Title: ", book, "\n"])
-        lookup_result.extend(["Cost: ", str(books[book]["cost"]), "\n"])
-        lookup_result.extend(["Amount: ", str(books[book]["amount"]), "\n"])
+        lookup_result.extend(["Price: ", str(books[book]["price"]), "\n"])
+        lookup_result.extend(["Stock: ", str(books[book]["stock"]), "\n"])
 
     # Return the lookup result in a string
     return "".join(lookup_result)
@@ -60,9 +60,9 @@ def buy(item_number):
 
     # Use the 'status' boolean in json to check if the purchase was successful
     if response["status"]:
-        return "Successfully purchased: " + response["title"] + "\n"
+        return "Successfully purchased: " + str(item_number) + "\n"
     else:
-        return "Failed to purchase: " + response["title"] + "\n"
+        return "Failed to purchase: " + str(item_number) + "\n"
 
 
 if __name__ == "__main__":
