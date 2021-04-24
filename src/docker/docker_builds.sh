adef1627a180b60d0c1acd1e8c524f6c9b075bf4
kdstevens <kdstevens@umass.edu>
2021-04-23 22:32:28 -0400

    Fixed comments in docker_build.sh

--- src/docker/docker_builds.sh
+++ src/docker/docker_builds.sh
@@ -3,10 +3,10 @@
 echo "Building frontend image..."
 docker build .. -f docker/frontend.Dockerfile -t frontend
 
-# This image should be run with "docker run order [name]" where name is order1, order2, etc
+# This image should be run with "docker run order NAME=[name]" where name is order1, order2, etc
 echo "Building order image..."
 docker build .. -f docker/order.Dockerfile -t order
 
-# This image should be run with "docker run catalog [name]" where name is catalog1, catalog2, etc
+# This image should be run with "docker run catalog NAME=[name]" where name is catalog1, catalog2, etc
 echo "Building catalog image..."
 docker build .. -f docker/catalog.Dockerfile -t catalog
